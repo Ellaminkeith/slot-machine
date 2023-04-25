2f818f6bf7075f5cc3416eb59123940d69f5fe83
ella <ellaminkeith@gmail.com>
2023-04-25 11:36:12 -0700

    function

--- script.js
+++ script.js
@@ -4,11 +4,19 @@ const Message = "";
 let credits = 10;
 let winning = false;
 
-const boards = document.querySelector('.boards');
-const spinBtn = document.querySelector('.spinBtn');
-const message = document.querySelector('.message');
+const boards = document.querySelector(".boards");
+const spinBtn = document.querySelector(".spinBtn");
+const message = document.querySelector(".message");
 
-spinBtn.addEventListener('click', spinBoards);
+spinBtn.addEventListener("click", spinBoards);
 function spinBoards() {
+  const generateNumbers = Math.floor(Math.random() * 9) + 1;
+  //   console.log(generateNumbers);
+  let boardsNumbers = [];
 
-}
+    if (
+      boardsNumbers[0] == boardsNumbers[1] ||
+      boardsNumbers[1] == boardsNumbers[2]
+    );
+    console.log(boardsNumbers);
+  }
