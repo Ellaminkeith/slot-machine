003e7752b94f4940b1e80f2eec2417e495b19d0d
ella <ellaminkeith@gmail.com>
2023-04-25 16:03:37 -0700

    js progress update

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const playerCredits = 0;
 const Message = "";
 
-let credits = 10;
+let credits = 20;
 let winning = false;
 
 const boards = document.querySelector(".boards");
@@ -10,25 +10,37 @@ const message = document.querySelector(".message");
 
 spinBtn.addEventListener("click", spinBoards);
 function spinBoards() {
-  const generateNumbers = Math.floor(Math.random() * 9) + 1;
-  //   console.log(generateNumbers);
+  if (credits === 0) {
+    message.innerText = "Game Over. Please Try Again!";
+    return;
+  }
+
   let boardsNumbers = [];
-  boardsNumbers.push(generateNumbers);
-    if (boardsNumbers.length === 3){
-        if (
-            boardsNumbers[0] === boardsNumbers[1] &&
-            boardsNumbers[1] === boardsNumbers[2]
-          ) {
-            winning = false;
-            credits = credits + 1;
-            message.innerHTML(message) = "Congrats, You WIN!"
-          } else (boardsNumbers[0] !== boardsNumbers[1] && 
-            boardsNumbers[1] !== boardsNumbers[2]
-          ) {
-            lost = true;
-              credits = credits - 1;
-              message.innerHTML(message) = "You Lost, Play Again!";
-          }
-        }
-}   
-// console.log(boardsNumbers)
+  for (let i = 0; i < 3; i++) {
+    const generateNumbers = Math.floor(Math.random() * 9) + 1;
+    boardsNumbers.push(generateNumbers);
+    // console.log(generateNumbers);
+  }
+
+  if (boardsNumbers.length === 3) {
+    if (
+      boardsNumbers[0] === boardsNumbers[1] &&
+      boardsNumbers[1] === boardsNumbers[2]
+    ) {
+      winning = false;
+      credits += 1;
+      message.innerText = "Congrats, You WIN!";
+    } else {
+      winning = true;
+      credits -= 1;
+      message.innerText = "You Lost, Play Again!";
+    }
+    console.log(boardsNumbers);
+  }
+}
+
+function playerCredits(boardsNumbers)
+if (credits === 0) {
+  playerCredits;
+  message.innerText = "No Credits, add more credits!";
+}
