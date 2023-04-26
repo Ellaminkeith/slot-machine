b547ad9d461c6d50396186fb77c210d4ec482900
ella <ellaminkeith@gmail.com>
2023-04-25 23:08:01 -0700

    css update

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-const playerCredits = 0;
+let playerCredits = 20;
 const Message = "";
 
 let boardsNumbers;
-let credits = 20;
 let winning = false;
 
 const board1 = document.querySelector(".board1");
@@ -10,10 +9,11 @@ const board2 = document.querySelector(".board2");
 const board3 = document.querySelector(".board3");
 const spinBtn = document.querySelector(".spinBtn");
 const message = document.querySelector(".message");
+const creditsScore = document.querySelector(".playerCredits");
 
 spinBtn.addEventListener("click", spinBoards);
 function spinBoards() {
-  if (credits === 0) {
+  if (playerCredits === 0) {
     message.innerText = "Game Over. Please Try Again!";
     return;
   }
@@ -23,36 +23,27 @@ function spinBoards() {
     const generateNumbers = Math.floor(Math.random() * 9) + 1;
     boardsNumbers.push(generateNumbers);
   }
-  render();
   console.log("boardsNumbers", boardsNumbers);
+
   if (
     boardsNumbers[0] === boardsNumbers[1] &&
     boardsNumbers[1] === boardsNumbers[2]
   ) {
-    winning = false;
-    credits += 1;
+    winning = true;
+    playerCredits += 1;
     message.innerText = "Congrats, You WIN!";
   } else {
-    winning = true;
-    credits -= 1;
+    winning = false;
+    playerCredits -= 1;
     message.innerText = "You Lost, Play Again!";
   }
+  render();
 }
 
 function render() {
   board1.innerText = boardsNumbers[0];
   board2.innerText = boardsNumbers[1];
   board3.innerText = boardsNumbers[2];
-}
-
-function renderCredits() {
-  if (winning) {
-    playerCredits += 1;
-  } else {
-    playerCredits -= 1;
-  }
-}
-
-function renderMessage() {
-    
+  creditsScore.innerText = playerCredits;
+  console.log(playerCredits);
 }
